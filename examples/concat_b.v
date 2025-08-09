1bba81f3beef7b1b298e8fa4b07497af821deec6
fermarsan <fmartinezsanta@gmail.com>
2025-08-08 20:12:34 -0500

    local parameters declaration supported

--- examples/concat_b.v
+++ examples/concat_b.v
@@ -3,6 +3,5 @@ module Concatenation #(parameter MAX = 8) (
     input [3:0] b,  // Entrada de 4 bits
     output [MAX-1:0] result // Salida de 8 bits (concatenación de a y b)
 );
-    // Concatenación de las dos entradas
-    assign result = {a, b, a}; // a se coloca a la izquierda y b a la derecha
+    assign result = {a, b, a}; 
 endmodule
